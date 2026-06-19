5918565c47d23c514cadd5994dcab6d5711b7945
Aapka Naam <aapka.email@example.com>
2026-06-20 00:36:16 +0530

    feat: Integrate Google Tag Manager (GTM) tracking tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KPJQFQ4X');`
+          }}
+        />
       </head>
       <body 
         className={`${inter.variable} ${playfair.variable}`}
@@ -53,6 +62,11 @@ export default function RootLayout({
           '--border': SITE_CONFIG.theme.border,
         } as React.CSSProperties}
       >
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KPJQFQ4X" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+          }}
+        />
         {children}
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-7K2DYR1YJ4" strategy="afterInteractive" />
         <Script
